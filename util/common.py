4534a6b438e3639b110555e11263f0edddc126c5
Alexander Kovalev <ray.kovalev@yandex.com>
2019-05-08 00:53:51 +0300

    webdriver fix

--- util/common.py
+++ util/common.py
@@ -14,7 +14,7 @@ def init_driver() -> webdriver:
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.headless = True
-    driver = webdriver.Chrome(options=options)
-    # driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=options)
     driver.set_window_size(1024, 768)
     return driver
